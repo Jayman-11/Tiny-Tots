475ee5eb41deac61d1cd305a0dc40027876ffd9d
Jaymar Bautista <124756188+Jayman-11@users.noreply.github.com>
2023-11-30 20:56:50 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<html>
+<header>
+    <title>Tiny Tots</title>
+    <?php  include "include/css-links.php" ?>
+    <?php require 'include/index-header.php'; ?>
+</header>
+
+<body>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-6"></div>
+            <div class="col-6 left-side d-flex flex-column align-items-center">
+                <div class="top-title text-center">
+                    <img src="assets/img/icon.png" class="" alt="Icon">
+                    <h2 class="text-white text-uppercase mt-2">Tiny Tots</h2>
+                </div>
+                <div class="bottom">
+                    <div class="w-100">
+                        <a href="parents/map.php" class="btn my-button text-uppercase">Find Preschool Near Me</a>
+                    </div>
+                    <hr class="bg-white my-5" />
+                    <div class="w-100">
+                        <a href="parents/Parent-Login.php" class="btn my-button text-uppercase">Login or Register</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php include "include/js-links.php" ?>
+    <!-- CHATBOT -->
+    <div>
+        <script async data-id="3483821588" id="chatling-embed-script" type="text/javascript"
+            src="https://chatling.ai/js/embed.js"></script>
+    </div>
+</body>
+
+</html>
