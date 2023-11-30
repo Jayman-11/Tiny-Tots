cc9cac01e3d3dd91ad5705c4e80c8f66f6e16c53
Jaymar Bautista <124756188+Jayman-11@users.noreply.github.com>
2023-11-30 21:02:58 +0800

    Add files via upload

---
+++ include/index-header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tiny Tots</title>
+    <link rel="stylesheet" href="css/navbar-styles.css">
+</head>
+
+<body>
+
+    <header>
+        <h2>Welcome to Tiny Tots!</h2>
+
+        <div class="navbar">
+            <a href="index.php" class="nav-item"><button>Tiny Tots</button></a>
+            <!-- <a href="map.php" class="nav-item"><button>Find Preschools Near Me</button></a> -->
+            <!-- <a href="Parent-Login.php" class="nav-item"><button>Login or Register</button></a> -->
+            <a href="school_admin/SCA-Login.php" class="nav-item"><button>School Admin Login</button></a>
+            <a href="teacher/Faculty-Login.php" class="nav-item"><button>Faculty Login</button></a>
+            <a href="web_admin/WA-Login.php" class="nav-item"><button>Web Admin Login</button></a>
+        </div>
+    </header>
+
+    <!-- CHATBOT -->
+    <div>
+        <script async data-id="3483821588" id="chatling-embed-script" type="text/javascript"
+            src="https://chatling.ai/js/embed.js"></script>
+    </div>
+
+</body>
+
+</html>
